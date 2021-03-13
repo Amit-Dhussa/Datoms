7b51f1f9e4a7c199c22f608bd36baedbb9b929d5
Amit Dhussa <62895271+Amit-Dhussa@users.noreply.github.com>
2021-03-13 16:07:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { render } from "@testing-library/react";
+
+
+function App() {
+  return (
+    <div className="App">
+      class MyForm extends React.component{
+      
+        <input type = "text"/>
+        
+      }
+    
+      ReactDom.render{
+        <EJ.Button id="btn" text ="Button" showRoundedCorner = {true}
+        size="large" text="Color Me"></EJ.Button>
+        // ReactDom.getElementById('btn');
+      }
+      
+      var Numbers = ["1","2","3","4","5","6","7","8","9"];
+
+
+      export default function SquareGridExample(props) {
+        <SquareGrid rows ={2} columns = {3}
+        items = {NUMBERS}
+        renderItem = {renderItem} />
+      }
+
+      function renderItem(item){
+        
+          <View style = {styles.item}>
+            <View style ={styles.content}>
+              <Text style ={styles.text}>{item}</Text>
+            </View>
+          </View>
+        
+      }
+     
+     class Foo extends Components{
+       constructor(props)
+         
+        
+       }
+       handleClick(){
+         numbers[String(inputvalue)].style.content.backgroundColor = "red"
+    
+       }
+       render(){
+         <button onClick={this.handleClick}>Click Me</button>
+       }
+     
+     
+
+    </div>
+  );
+}
+
+export default App;
